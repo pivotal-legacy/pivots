17fd0bee4ce5330b76809c1182c8f2efc3867a10
Whitney Schaefer <wschaefer@pivotal.io>
2015-09-10 13:40:33 +0900

    Add basic router setup
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,24 @@
 'use strict';
 
 var React = require('react');
-var App = require('./components/App');
+var Router = require('react-router');
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
 
+var App = require('./components/App');
+var Login = require('./components/Login');
+
+// CSS
 require('normalize.css');
 require('./../css/main.css');
 
-React.render(<App/>, document.getElementById('content'));
+var routes = (
+  <Route>
+    <DefaultRoute handler={App}/>
+    <Route name="login" handler={Login}/>
+  </Route>
+);
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById('content'));
+});
