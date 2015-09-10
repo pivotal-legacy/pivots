05b7475c9ee813abd85b85883d80a7a0ed77d2f7
Whitney Schaefer <wschaefer@pivotal.io>
2015-09-10 11:51:23 +0900

    Add live-reload
    Signed-off-by: Eno Compton <ecompton@pivotal.io>

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,15 @@ var connect = require('gulp-connect');
 
 gulp.task('html', ['clean'], function () {
   return gulp.src(['./src/index.html'])
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('js', ['clean'], function () {
   return gulp.src('src/js/main.js')
     .pipe(webpack(require('./webpack.config.js')))
-    .pipe(gulp.dest('dist/js/'));
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('watch', ['build'], function() {
@@ -21,7 +23,7 @@ gulp.task('watch', ['build'], function() {
 });
 
 gulp.task('connect', function() {
-  connect.server({root: 'dist'});
+  connect.server({root: 'dist', livereload: true});
 });
 
 gulp.task('clean', function () {
@@ -30,4 +32,4 @@ gulp.task('clean', function () {
 
 gulp.task('build', ['clean', 'html', 'js']);
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['build', 'connect', 'watch']);
--- src/js/components/App.js
+++ src/js/components/App.js
@@ -5,7 +5,7 @@ var React = require('react');
 var App = React.createClass({
   render: () => {
     return (
-      <h1>Hi There, Everyone!</h1>
+      <h1>Hello, World!</h1>
     );
   }
 });
