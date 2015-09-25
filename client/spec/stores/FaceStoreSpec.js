64cf430d9ae44bf7bf77fa55f2fcab45a9c0fda3
Eno Compton <ecompton@pivotal.io>
2015-09-25 13:59:22 +0900

    Add test for FaceStore#fetchAll
    Signed-off-by: Yuki Nishijima <ynishijima@pivotal.io>

--- client/spec/stores/FaceStoreSpec.js
+++ client/spec/stores/FaceStoreSpec.js
@@ -2,70 +2,92 @@
 
 describe('FaceStore', function () {
   var FaceStore = require('../../src/js/stores/FaceStore');
+  var SpecHelper = require('../specHelper');
+  var Api = require('../../src/js/utils/Api');
   var whitney, danny, markM, markD;
 
-  beforeEach(function () {
-    whitney = {firstName: 'Whitney', lastName: 'Schaefer'};
-    danny = {firstName: 'Danny', lastName: 'Burkes'};
-    markM = {firstName: 'Mark', lastName: 'Macdonals'};
-    markD = {firstName: 'Mark', lastName: 'Dellilo'};
+  describe('#fetchAll', function () {
+    it('makes a request to Api', function () {
+      var fakePromise = SpecHelper.buildFakePromise();
+      spyOn(Api, 'get').and.returnValue(fakePromise);
+      spyOn(FaceStore, 'trigger');
 
-    FaceStore.faces = [danny, whitney, markM, markD];
+      FaceStore.fetchAll();
+
+      expect(Api.get).toHaveBeenCalledWith('/employees');
+
+      var faces = [{name: 'Danny'}];
+      fakePromise.resolve({data: faces});
+
+      expect(FaceStore.faces).toEqual(faces);
+      expect(FaceStore.trigger).toHaveBeenCalledWith(faces);
+    });
   });
 
-  it('filters faces by first name', function () {
-    spyOn(FaceStore, 'trigger');
+  describe('#search', function () {
+    beforeEach(function () {
+      whitney = {firstName: 'Whitney', lastName: 'Schaefer'};
+      danny = {firstName: 'Danny', lastName: 'Burkes'};
+      markM = {firstName: 'Mark', lastName: 'Macdonals'};
+      markD = {firstName: 'Mark', lastName: 'Dellilo'};
 
-    FaceStore.search('Danny');
+      FaceStore.faces = [danny, whitney, markM, markD];
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([danny]);
-  });
+    it('filters faces by first name', function () {
+      spyOn(FaceStore, 'trigger');
 
-  it('filters faces by first name', function () {
-    spyOn(FaceStore, 'trigger');
+      FaceStore.search('Danny');
 
-    FaceStore.search('Dan');
+      expect(FaceStore.trigger).toHaveBeenCalledWith([danny]);
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([danny]);
-  });
+    it('filters faces by first name', function () {
+      spyOn(FaceStore, 'trigger');
 
-  it('filters by last name', function () {
-    spyOn(FaceStore, 'trigger');
+      FaceStore.search('Dan');
 
-    FaceStore.search('Schaefer');
+      expect(FaceStore.trigger).toHaveBeenCalledWith([danny]);
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([whitney]);
-  });
+    it('filters by last name', function () {
+      spyOn(FaceStore, 'trigger');
 
-  it('is case insensitive', function () {
-    spyOn(FaceStore, 'trigger');
+      FaceStore.search('Schaefer');
 
-    FaceStore.search('danny');
+      expect(FaceStore.trigger).toHaveBeenCalledWith([whitney]);
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([danny]);
-  });
+    it('is case insensitive', function () {
+      spyOn(FaceStore, 'trigger');
 
-  it('returns everyone if search is a empty string', function() {
-    spyOn(FaceStore, 'trigger');
+      FaceStore.search('danny');
 
-    FaceStore.search('');
+      expect(FaceStore.trigger).toHaveBeenCalledWith([danny]);
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([danny, whitney, markM, markD]);
-  });
+    it('returns everyone if search is a empty string', function () {
+      spyOn(FaceStore, 'trigger');
 
-  it('returns everyone if search is undefined', function() {
-    spyOn(FaceStore, 'trigger');
+      FaceStore.search('');
 
-    FaceStore.search(undefined);
+      expect(FaceStore.trigger).toHaveBeenCalledWith([danny, whitney, markM, markD]);
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([danny, whitney, markM, markD]);
-  });
+    it('returns everyone if search is undefined', function () {
+      spyOn(FaceStore, 'trigger');
 
-  it('returns multiple pivots if search matches multiple names', function() {
-    spyOn(FaceStore, 'trigger');
+      FaceStore.search(undefined);
 
-    FaceStore.search('Mark');
+      expect(FaceStore.trigger).toHaveBeenCalledWith([danny, whitney, markM, markD]);
+    });
 
-    expect(FaceStore.trigger).toHaveBeenCalledWith([markM, markD]);
+    it('returns multiple pivots if search matches multiple names', function () {
+      spyOn(FaceStore, 'trigger');
+
+      FaceStore.search('Mark');
+
+      expect(FaceStore.trigger).toHaveBeenCalledWith([markM, markD]);
+    });
   });
 });
