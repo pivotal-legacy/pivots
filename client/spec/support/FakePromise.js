0cbeff5252f5b4c9520984e77d1986ffa0d7f6de
Eno Compton 4 <eno4@ecom.com>
2015-09-27 12:48:19 +0900

    Convert FakePromise to be a new-able object
    Rather than use FakePromise as a factory, this commit allows consumers
    of the FakePromise to new it up.

--- client/spec/support/FakePromise.js
+++ client/spec/support/FakePromise.js
@@ -1,24 +1,22 @@
-export default () => {
-  let realSuccessCallback;
-  let realFailureCallback;
+function FakePromise() {
+}
 
-  return {
-    then(f) {
-      realSuccessCallback = f;
-      return this;
-    },
-
-    fail(f) {
-      realFailureCallback = f;
-      return this;
-    },
-
-    resolve(payload) {
-      return realSuccessCallback(payload);
-    },
-
-    reject(payload) {
-      return realFailureCallback(payload);
-    }
-  };
+FakePromise.prototype.then = function (f) {
+  this.realSuccessCallback = f;
+  return this;
 };
+
+FakePromise.prototype.fail = function (f) {
+  this.realFailureCallback = f;
+  return this;
+};
+
+FakePromise.prototype.resolve = function (payload) {
+  return this.realSuccessCallback(payload);
+};
+
+FakePromise.prototype.reject = function (payload) {
+  return this.realFailureCallback(payload);
+};
+
+export default FakePromise;
