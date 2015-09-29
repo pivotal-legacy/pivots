5b3544e1724b982f04d3b11c6e752cd9ed4c4b45
Eno Compton 4 <eno4@ecom.com>
2015-09-29 20:14:04 +0900

    Improve CORS filter
    Prior to this commit, we were sending back all CORS-related headers on
    all requests. In fact, some of the headers are required in response
    to only an OPTIONS request.
    
    The headers required only for the pre-flight OPTIONS requests are:
    
    Access-Control-Allow-Methods
    Access-Control-Max-Age
    Access-Control-Allow-Headers
    
    Meanwhile, all API responses should include the following headers:
    
    Access-Control-Allow-Origin
    Access-Control-Expose-Headers
    
    For more information, see the documentation at MDN:
    
    https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS

--- server/applications/pivots/src/main/java/io.pivotal.pivotsapp/CORSFilter.java
+++ server/applications/pivots/src/main/java/io.pivotal.pivotsapp/CORSFilter.java
@@ -1,14 +1,11 @@
 package io.pivotal.pivotsapp;
 
-import java.io.IOException;
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Component
 public class CORSFilter implements Filter {
@@ -18,19 +15,32 @@ public class CORSFilter implements Filter {
     public static final String ALLOWED_ORIGIN = "*";
     public static final String ALLOWED_HEADERS = "Content-Type, Accept, X-AUTH-TOKEN";
     public static final String EXPOSED_HEADERS = "X-AUTH-TOKEN";
+    public static final String OPTIONS = "OPTIONS";
 
+    // Note that some of the following headers are used only
+    // for responses to preflight requests, i.e., those added to OPTIONS requests.
+    //
+    // For details on the use of the following headers, see here:
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
+
         response.setHeader("Access-Control-Allow-Origin", ALLOWED_ORIGIN);
-        response.setHeader("Access-Control-Allow-Methods", ALLOWED_METHODS);
-        response.setHeader("Access-Control-Max-Age", CACHE_TIME);
-        response.setHeader("Access-Control-Allow-Headers", ALLOWED_HEADERS);
         response.setHeader("Access-Control-Expose-Headers", EXPOSED_HEADERS);
+
+        if (request.getMethod().equals(OPTIONS)) {
+            response.setHeader("Access-Control-Allow-Methods", ALLOWED_METHODS);
+            response.setHeader("Access-Control-Max-Age", CACHE_TIME);
+            response.setHeader("Access-Control-Allow-Headers", ALLOWED_HEADERS);
+        }
 
         chain.doFilter(req, res);
     }
 
-    public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {
+    }
 
-    public void destroy() {}
+    public void destroy() {
+    }
 }
